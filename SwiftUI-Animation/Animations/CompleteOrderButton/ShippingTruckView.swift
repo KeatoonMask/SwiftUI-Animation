20f8cf18cd74bad34cd45a47a0cc627f0208f9ba
KeatoonMask <ennes.finsi@gmail.com>
2020-12-19 14:16:41 +0100

    Update README.md

---
+++ SwiftUI-Animation/Animations/CompleteOrderButton/ShippingTruckView.swift
@@ -0,0 +1,20 @@
+//
+//  ShippingTruckView.swift
+//  SwiftUI-Animation
+//
+//  Created by Serafín Ennes Moscoso on 19/12/2020.
+//
+
+import SwiftUI
+
+struct ShippingTruckView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ShippingTruckView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShippingTruckView()
+    }
+}
