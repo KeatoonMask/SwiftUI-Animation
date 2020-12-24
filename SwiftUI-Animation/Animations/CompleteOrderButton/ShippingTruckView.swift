0ddb2c4102e55e278eca95f7f4feaa2d7660b88a
KeatoonMask <ennes.finsi@gmail.com>
2020-12-24 12:06:25 +0100

    Create complementary view (ShippingTruckView) for the order button

--- SwiftUI-Animation/Animations/CompleteOrderButton/ShippingTruckView.swift
+++ SwiftUI-Animation/Animations/CompleteOrderButton/ShippingTruckView.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct ShippingTruckView: View {
+
+    @State public var isPlaced = false
+    @State public var isShipped = false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            HStack {
+                Image(systemName: "cube.box")
+                    .resizable()
+                    .opacity(isPlaced ? 1 : 0)
+                    .frame(width: 35, height: 35)
+                    .animation(Animation.linear(duration: 0.3))
+                Spacer()
+                    .frame(width: isShipped ? 35 : 200)
+                    .animation(Animation.easeInOut(duration: 0.3).delay(1))
+
+            }
+
+            Image("truck")
+                .resizable()
+                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 100)
+        }.onTapGesture {
+            startShipping()
+        }
+    }
+
+    func startShipping() {
+        isPlaced.toggle()
+        Timer.scheduledTimer(withTimeInterval: 0.6, repeats: false) { timer in
+            isShipped.toggle()
+        }
+    }
+
+    public func makeOrder() {
+        isPlaced.toggle()
+        Timer.scheduledTimer(withTimeInterval: 0.6, repeats: false) { timer in
+            isShipped.toggle()
+        }
     }
 }
 
