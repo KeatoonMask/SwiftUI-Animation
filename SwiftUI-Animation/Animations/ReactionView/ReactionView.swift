2886438c855fd104cb7053bee0d91739bb05dc0e
Finsi Ennes <ennes.finsi@gmail.com>
2022-09-08 18:37:33 +0200

    feat: Improve ReactionView implementation. Some details are pending

--- SwiftUI-Animation/Animations/ReactionView/ReactionView.swift
+++ SwiftUI-Animation/Animations/ReactionView/ReactionView.swift
@@ -9,44 +9,29 @@ import SwiftUI
 
 struct ReactionView: View {
 
-    var iconSize: CGFloat
-    @State private var showReaction = false
+    @Binding var showReaction: Bool
 
-    private let animation: Animation = .interpolatingSpring(mass: 0.5, stiffness: 100, damping: 6)
+    let lightBackground = Color(red: 240/255, green: 240/255, blue: 245/255)
 
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 20)
-                .fill(darkGray)
-                .frame(width: iconSize * 5 + 140, height: iconSize + 30)
+                .frame(width: 44 * 5 + 140, height: 44 + 30)
+                .foregroundColor(lightBackground)
                 .scaleEffect(showReaction ? 1 : 0, anchor: .center)
-                .animation(.interpolatingSpring(mass: 0.5, stiffness: 100, damping: 10), value: showReaction)
+                .animation(.interpolatingSpring(mass: 0.5, stiffness: 100, damping: 10).delay(showReaction ? 0 : 0.3), value: showReaction)
 
             HStack(spacing: 20) {
-                Image(uiImage: "👍".textToImage())
-                    .rotationEffect(.degrees(showReaction ? 0 : -45))
-                    .scaleEffect(showReaction ? 1 : 0, anchor: .bottomLeading)
-                    .animation(animation.delay(0.3), value: showReaction)
+                ReactionIconView(icon: "👍", show: $showReaction, degrees: -45)
 
-                Image(uiImage: "👎".textToImage())
-                    .rotationEffect(.degrees(showReaction ? 0 : -45))
-                    .scaleEffect(showReaction ? 1 : 0, anchor: .bottomLeading)
-                    .animation(animation.delay(0.3), value: showReaction)
+                ReactionIconView(icon: "👎", show: $showReaction, degrees: -45)
 
-                Image(uiImage: "❤️".textToImage())
-                    .scaleEffect(showReaction ? 1 : 0, anchor: .bottomLeading)
-                    .animation(animation.delay(0.3), value: showReaction)
+                ReactionIconView(icon: "❤️", show: $showReaction)
 
-                Image(uiImage: "😂".textToImage())
-                    .offset(x: showReaction ? 0 : 40)
-                    .scaleEffect(showReaction ? 1 : 0, anchor: .bottomLeading)
-                    .animation(animation.delay(0.3), value: showReaction)
+                ReactionIconView(icon: "😂", show: $showReaction, offset: 40)
 
-                ReactionIconView(icon: "😠", show: $showReaction)
+                ReactionIconView(icon: "😠", show: $showReaction, offset: 40)
             }
-            .onAppear{
-                showReaction.toggle()
-        }
         }
     }
 }
@@ -54,22 +39,62 @@ struct ReactionView: View {
 struct ReactionIconView: View {
 
     var icon: String
+    var degrees: Double
+    var offset: CGFloat
+    
     @Binding var show: Bool
 
     private var iconImage: UIImage { icon.textToImage() }
     private let animation: Animation = .interpolatingSpring(mass: 0.5, stiffness: 100, damping: 6)
 
+
+    public init(icon: String,
+                show: Binding<Bool>,
+                degrees: Double = 0,
+                offset: CGFloat = 0.0) {
+        self.icon = icon
+        self.degrees = degrees
+        self.offset = offset
+        self._show = show
+    }
+
     var body: some View {
         Image(uiImage: iconImage)
-            .offset(x: show ? 0 : 40)
-            .scaleEffect(show ? 1 : 0, anchor: .bottomLeading)
-            .animation(animation.delay(0.3), value: show)
+            .rotationEffect(.degrees(show ? 0 : degrees))
+            .offset(x: show ? 0 : offset)
+            .scaleEffect(show ? 1 : 0)
+            .animation(show ? animation.delay(show ? 0.3 : 0) : Animation.linear, value: show)
+    }
+}
+
+struct ReactionViewContainer: View {
+    @State private var showReaction = false
+
+    var body: some View {
+        VStack(spacing: 20) {
+            ReactionView(showReaction: $showReaction)
+
+            Button(action: {
+                showReaction.toggle()
+            }) {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 12.0)
+                        .frame(width: 80, height: 70, alignment: .center)
+                        .foregroundColor(darkGreen)
+
+                    Image(systemName: "power")
+                        .resizable()
+                        .frame(width: 44, height: 44)
+                        .foregroundColor(.white)
+                }
+            }
+        }
     }
 }
 
 struct ReactionView_Previews: PreviewProvider {
     static var previews: some View {
-        ReactionView(iconSize: 44)
+        ReactionViewContainer()
     }
 }
 
