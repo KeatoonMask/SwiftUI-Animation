97e9d20b0a8632878486cf6b6fbb2b0f0221b332
Finsi Ennes <ennes.finsi@gmail.com>
2022-11-14 19:22:27 +0100

    Add percentage feature to battery animations

--- SwiftUI-Animation/Animations/Battery/BatteryView.swift
+++ SwiftUI-Animation/Animations/Battery/BatteryView.swift
@@ -9,7 +9,21 @@ import SwiftUI
 
 struct BatteryView: View {
 
+    public let percentage: CGFloat
     public let width: CGFloat
+
+    private var batteryWidth: CGFloat {
+        width * 0.725
+    }
+
+    private var batteryTrailing: CGFloat {
+        let min = width * 0.6
+        let max = width * 1.3
+        let scale = max - min
+
+        return min + (scale * (1.0 - percentage))
+    }
+
     var body: some View {
         VStack {
 
@@ -18,20 +32,28 @@ struct BatteryView: View {
                 .frame(width: width, height: width / 3)
 
             ZStack {
-                    Image(systemName: "battery.0")
-                        .resizable()
-                        .frame(width: width, height: width / 3)
+                Image(systemName: "battery.0")
+                    .resizable()
+                    .frame(width: width, height: width / 3)
+
+                HStack {
+                    Spacer()
+                        .frame(width: width * 0.6)
 
                     RoundedRectangle(cornerRadius: 16.0)
-                        .frame(width: width * 0.725, height: (width / 3) * 0.65)
-                        .offset(x: -11)
+                        .frame(height: (width / 3) * 0.65)
+                    .offset(x: -width * 0.055)
+
+                    Spacer()
+                        .frame(width: batteryTrailing) // 120 - 1.3
+                }
 
                 Image(systemName: "bolt")
                     .resizable()
                     .frame(width: width * 0.4, height: width * 0.36)
                     .foregroundColor(.white)
                     .font(Font.title.weight(.light))
-                    .offset(x: -11)
+                    .offset(x: -width * 0.055)
             }
         }
     }
@@ -39,6 +61,6 @@ struct BatteryView: View {
 
 struct BatteryView_Previews: PreviewProvider {
     static var previews: some View {
-        BatteryView(width: 200)
+        BatteryView(percentage: 0.75, width: 200)
     }
 }
