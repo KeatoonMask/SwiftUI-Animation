9fb5091e82e7e568fc1c36d976616ae0bd3c9a56
Finsi Ennes <ennes.finsi@gmail.com>
2022-11-15 18:57:53 +0100

    Include animations on battery view

--- SwiftUI-Animation/Animations/Battery/BatteryView.swift
+++ SwiftUI-Animation/Animations/Battery/BatteryView.swift
@@ -9,27 +9,26 @@ import SwiftUI
 
 struct BatteryView: View {
 
-    public let percentage: CGFloat
+    @Binding public var isCharging: Bool
     public let width: CGFloat
+
+    private var height: CGFloat { width * 0.333 }
 
     private var batteryWidth: CGFloat {
         width * 0.725
     }
 
-    private var batteryTrailing: CGFloat {
-        let min = width * 0.6
-        let max = width * 1.3
-        let scale = max - min
+    private var batteryMargin: CGFloat { width * 0.6 }
 
-        return min + (scale * (1.0 - percentage))
-    }
+    private var batteryOffset: CGFloat { -width * 0.055 }
+
 
     var body: some View {
         VStack {
 
             Image(systemName: "battery.100.bolt")
                 .resizable()
-                .frame(width: width, height: width / 3)
+                .frame(width: width, height: height)
 
             ZStack {
                 Image(systemName: "battery.0")
@@ -38,29 +37,70 @@ struct BatteryView: View {
 
                 HStack {
                     Spacer()
-                        .frame(width: width * 0.6)
+                        .frame(width: batteryMargin)
 
                     RoundedRectangle(cornerRadius: 16.0)
-                        .frame(height: (width / 3) * 0.65)
-                    .offset(x: -width * 0.055)
+                        .frame(width: isCharging ? width * 0.75 : width * 0.1, height: (width / 3) * 0.65)
+                        .animation(.linear(duration: 2.5))
+                        .offset(x: batteryOffset)
 
                     Spacer()
-                        .frame(width: batteryTrailing) // 120 - 1.3
                 }
 
-                Image(systemName: "bolt")
-                    .resizable()
-                    .frame(width: width * 0.4, height: width * 0.36)
-                    .foregroundColor(.white)
-                    .font(Font.title.weight(.light))
-                    .offset(x: -width * 0.055)
+                BoltView(size: width)
+                    .opacity(isCharging ? 1.0 : 0.0)
+                    .animation(.linear(duration: 0.3))
             }
+        }
+    }
+}
+
+struct BoltView: View {
+    public let size: CGFloat
+
+    private var width: CGFloat { size * 0.4 }
+    private var height: CGFloat { size * 0.36 }
+
+    var body: some View {
+        ZStack {
+            Image(systemName: "bolt.fill")
+                .resizable()
+                .frame(width: width, height: height)
+                .foregroundColor(.black)
+                .font(Font.title.weight(.light))
+
+            Image(systemName: "bolt")
+                .resizable()
+                .frame(width: width, height: height)
+                .foregroundColor(.white)
+                .font(Font.title.weight(.light))
+        }
+    }
+}
+
+struct BatteryViewContainer: View {
+    @State private var percentage: CGFloat = 0.4
+    @State private var isCharging = false
+
+    var body: some View {
+        VStack {
+            BatteryView(isCharging: $isCharging, width: 200)
+
+            Button(action: {
+                isCharging.toggle()
+            }, label: {
+                HStack {
+
+                    RoundedRectangle(cornerRadius: 16.0)
+                        .frame(height: 44)
+                }
+            })
         }
     }
 }
 
 struct BatteryView_Previews: PreviewProvider {
     static var previews: some View {
-        BatteryView(percentage: 0.75, width: 200)
+        BatteryViewContainer()
     }
 }
