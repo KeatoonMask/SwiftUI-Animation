619935bda53690c21d225a9cf87ee869ef931928
Finsi Ennes <ennes.finsi@gmail.com>
2023-04-11 23:24:47 +0200

    feat: Add icons and color animations to the NavInteraction

--- SwiftUI-Animation/Animations/NavInteraction/NavInteraction.swift
+++ SwiftUI-Animation/Animations/NavInteraction/NavInteraction.swift
@@ -12,26 +12,47 @@ struct NavInteractionItem: View {
     private let animation: Animation = .interpolatingSpring(mass: 0.5, stiffness: 100, damping: 6)
     
     @Binding var isSelected: Bool
+    var image: String
 
     var body: some View {
-        RoundedRectangle(cornerRadius: 50)
-            .frame(width: isSelected ? 100 : 50, height: 50)
-            .foregroundColor(.gray)
-            .animation(animation, value: isSelected)
+        ZStack {
+            RoundedRectangle(cornerRadius: 50)
+                .frame(width: isSelected ? 80 : 50, height: 50)
+                .animation(animation, value: isSelected)
+                .foregroundColor(.white)
+                .opacity(isSelected ? 0.5 : 1)
+
+
+            Image(systemName: image)
+                .resizable()
+                .frame(width: 25, height: 25)
+                .foregroundColor(isSelected ? .white : .gray)
+                .animation(animation, value: isSelected)
+        }
     }
 }
 
 struct NavInteraction: View {
 
     @State private var isSelected = [true, false, false, false]
+    @State private var navColor = darkBlue
+
+    private var colors: [Color] = [darkBlue, darkPink, darkGray, darkViolet]
+    private var image = ["person.fill", "magnifyingglass", "heart.fill", "gearshape.fill"]
 
     var body: some View {
-        HStack {
-            ForEach(0..<4) { i in
-                NavInteractionItem(isSelected: $isSelected[i])
-                    .onTapGesture {
-                        select(item: i)
-                    }
+        ZStack {
+            RoundedRectangle(cornerRadius: 50)
+                .frame(width: 300, height: 70)
+                .foregroundColor(navColor)
+
+            HStack {
+                ForEach(0..<4) { i in
+                    NavInteractionItem(isSelected: $isSelected[i], image: image[i])
+                        .onTapGesture {
+                            select(item: i)
+                        }
+                }
             }
         }
         .animation(.linear(duration: 0.1))
@@ -39,6 +60,7 @@ struct NavInteraction: View {
     }
 
     func select(item: Int) {
+        navColor = colors[item]
         isSelected = [false, false, false, false]
         isSelected[item] = true
     }
